03c38a14a69eee1e5454d82b92adeb8bd48f290e
Parthiv-tech-git <dparthivkumar@gmail.com>
2025-11-21 13:55:20 +0530

    ch

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,35 @@
-
+import React from 'react';
 import './App.css';
-import { useState } from 'react';
+
 
 
 function App() {
- const[datali,setdatli]=useState('')
+
  const gvalue=(event)=>{
   console.log(event.target.value)
- }
+
+}
+  
+  const re=()=>{
+if(gvalue ==""){
+    alert("fill the all blanks")
+
+  }
+  else{
+    alert('sucessfull you goog data')
+  }
+  }
+ 
 
   return (
     <div id='ma'>
       <h1 id='h'>simple interest calculater</h1>
       <div id='mi'>
         <h1 id='v'> parthiv kumsart </h1>
-        <input type="numder"  id="p" placeholder='enter your principle amount ' onChange={gvalue}   /> <br /> 
-        <input type="number"  id="r" placeholder='rate of interset ' /><br />
-        <input type="number" name="" id="t" placeholder='duration' /> <br />
-<button id='s' >sumbit</button>
+        <input type="numder"  id="p" placeholder='enter your principle amount ' onChange={gvalue}    /> <br /> 
+        <input type="number"  id="r" placeholder='rate of interset '  onChange={gvalue}/><br />
+        <input type="number" name="" id="t" placeholder='duration' onChange={gvalue} /> <br />
+<button id='s' onClick={re} >sumbit</button>
       </div>
     </div>
       
