04fef4c5bb1599ef1e81a09f6b50af082c45a42a
Parthiv-tech-git <dparthivkumar@gmail.com>
2025-11-22 15:55:52 +0530

    code

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import './App.css';
-
+import { useState } from 'react';
 
 
 function App() {
+ const [principal,setPrincipal]=useState('');
+ const[rate,setRate]=useState('');
+ const[time,setTime]=useState(''); 
+    const [simpleInterest, setSimpleInterest] = useState(null);
 
- const gvalue=(event)=>{
-  console.log(event.target.value)
-
-}
-  
-  const re=()=>{
-if(gvalue ==""){
-    alert("fill the all blanks")
-
-  }
-  else{
-    alert('sucessfull you goog data')
-  }
+  const calculate =()=>{
+    const p = parseInt(principal);
+    const r = parseInt(rate);
+    const t = parseInt(time);
+   if(!isNaN(p)&&!isNaN(r)&&!isNaN(t)&& p>=0 && r>=0 && t>=0 ) {
+    
+    const result =(p*r*t)/100;
+    setSimpleInterest(calculate); 
+   }
+   else{
+     alert('Please enter valid positive numbers for Principal, Rate, and Time.');
+      setSimpleInterest(null);
+   }
   }
  
 
@@ -26,10 +30,10 @@ if(gvalue ==""){
       <h1 id='h'>simple interest calculater</h1>
       <div id='mi'>
         <h1 id='v'> parthiv kumsart </h1>
-        <input type="numder"  id="p" placeholder='enter your principle amount ' onChange={gvalue}    /> <br /> 
-        <input type="number"  id="r" placeholder='rate of interset '  onChange={gvalue}/><br />
-        <input type="number" name="" id="t" placeholder='duration' onChange={gvalue} /> <br />
-<button id='s' onClick={re} >sumbit</button>
+        <input type="numder"  value={prinicpal} id="p" placeholder='enter your principle amount ' onChange= {(e)=>{setPrincipal(e.target.value)}    /> <br /> 
+        <input type="number"  value={rate} id="r" placeholder='rate of interset '  onChange={(e)=>{setRate(e.target.value)}/> <br />
+        <input type="number" name="" id="t" value={time} placeholder='duration' onChange={(e)=>{setTime(e.target.value)} /> <br />
+<button id='s' onClick={calculate} >sumbit</button>
       </div>
     </div>
       
